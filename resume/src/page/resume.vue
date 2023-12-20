d1f302d6148332f2fd0aa9b88d0c015ccf6abffe
zhangyan <zhangy-ej@glodon.com>
2023-12-20 16:34:14 +0800

    feat:教育和工作经历

--- resume/src/page/resume.vue
+++ resume/src/page/resume.vue
@@ -4,7 +4,7 @@
       <img src="../assets/pic.jpg" alt="" style="width: 125px;height:125px;border-radius:65px;margin-top:50px">
       <p style="font-size: 24px;font-weight: 700">张燕</p>
       <p>前端开发</p>
-      <div style="text-align: left;margin-left: 5px;">
+      <div style="text-align: left;">
         <div><i class="el-icon-phone-outline left-icon"></i>1510108xxxx</div>
         <div><i class="el-icon-message left-icon"></i>keepamiss@163.com</div>
         <div><i class="el-icon-location-information left-icon"></i>北京</div>
@@ -12,10 +12,22 @@
       </div>
 
       <div>
+        <h2 style="color: #56CCF2;">工作经历</h2>
+        <div style="text-align:left">
+          <p>2021.12 ~ 至今</p>
+          <div class="flex-space"><span>广联达科技股份有限公司 - 高级前端开发工程师</span></div>
+          <p>2017.07 ~ 2021.09</p>
+          <div class="flex-space"><span>北京星网锐捷网络技术有限公司 - 前端开发工程师</span></div>
+        </div>
+      </div>
+
+      <div>
         <h2 style="color: #56CCF2;">教育经历</h2>
         <div style="text-align:left">
-        <p><span>天津工业大学</span><span>软件工程</span><span>2015.09 - 2017.07</span></p>
-        <p><span>天津大学</span><span>计算机信息管理</span><span>2012.09 - 2015.07</span></p>
+          <p>2015.09 ~ 2017.07</p>
+          <div class="flex-space"><span>天津工业大学 - 软件工程专业</span></div>
+          <p>2012.09 ~ 2015.07</p>
+          <div class="flex-space"><span>天津大学 - 计算机信息管理专业</span></div>
         </div>
       </div>
 
@@ -38,41 +50,48 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'resumePage',
-    data() {
-      return {}
-    }
+export default {
+  name: 'resumePage',
+  data() {
+    return {}
   }
+}
 </script>
 <style scoped lang="scss">
-  .content {
-    display: flex;
+.content {
+  display: flex;
+  height: 100%;
+  width: 1024px;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: 0 0 5px #ccc;
+  border-radius: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 14px;
+  position: relative;
+
+  .left {
     height: 100%;
-    width: 1024px;
-    overflow: hidden;
-    background-color: #fff;
-    box-shadow: 0 0 5px #ccc;
-    border-radius: 10px;
-    margin-left: auto;
-    margin-right: auto;
-    font-size: 14px;
-    position: relative;
+    flex-basis: 250px;
+    background: #174180;
+    color: white;
+    padding: 10px;
 
-    .left {
-      height: 100%;
-      flex-basis: 250px;
-      background: #174180;
-      color: white;
-
-      .left-icon {
-        margin-right: 5px;
-      }
-    }
-
-    .right {
-      height: 100%;
-      flex: 1;
+    .left-icon {
+      margin-right: 5px;
     }
   }
+
+  .right {
+    height: 100%;
+    flex: 1;
+    margin-left: 5px;
+  }
+  .flex-space{
+    display: flex;
+    justify-content: space-between;
+  }
+
+}
 </style>
